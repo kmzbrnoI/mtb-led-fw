10da0ab4bb8013fac2b6b67537b1b81a975cca43
Jan Malina <me@apophis.cz>
2026-06-10 18:37:15 +0200

    Bootloader: remove unused functions.

--- bootloader/src/io.h
+++ bootloader/src/io.h
@@ -8,29 +8,34 @@
 #include <stdint.h>
 #include <avr/io.h>
 
-#define PIN_LED_RED PC0
+#define PIN_LED_RED PD7
 #define PIN_LED_GREEN PB0
 #define PIN_LED_BLUE PB1
 
 #define PIN_BUTTON PD5
-#define PIN_TEST_PAD PD4
-#define PIN_INPUT_SHIFT PD7
-#define PIN_OUTPUTS_DISABLE PD6
 
 #define PIN_UART_RX PD0
 #define PIN_UART_TX PD1
-#define PIN_UART_DIR PD2
+#define PIN_UART_DIR PD3
 
-void io_init();
+void io_init(void) {
+	DDRB = (1 << PIN_LED_GREEN) | (1 << PIN_LED_BLUE);
+	DDRC = 0;
+	DDRD = (1 << PIN_LED_RED);
+	DDRE = 0;
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = (1 << PIN_BUTTON) | (1 << PIN_UART_RX); // pull-up
+	PORTE = 0;
+	// DDRD |= (1 << PIN_UART_DIR) | (1 << PIN_UART_TX); // TODO is it required?
+}
 
-uint8_t io_get_addr_raw();
-
-static inline void io_led_red_on() { PORTC &= ~(1 << PIN_LED_RED); }
-static inline void io_led_red_off() { PORTC |= (1 << PIN_LED_RED); }
-static inline void io_led_green_on() { PORTB |= (1 << PIN_LED_GREEN); }
-static inline void io_led_green_off() { PORTB &= ~(1 << PIN_LED_GREEN); }
-static inline void io_led_blue_on() { PORTB |= (1 << PIN_LED_BLUE); }
-static inline void io_led_blue_off() { PORTB &= ~(1 << PIN_LED_BLUE); }
+static inline void io_led_red_on(void) { PORTD &= ~(1 << PIN_LED_RED); }
+static inline void io_led_red_off(void) { PORTD |= (1 << PIN_LED_RED); }
+static inline void io_led_green_on(void) { PORTB |= (1 << PIN_LED_GREEN); }
+static inline void io_led_green_off(void) { PORTB &= ~(1 << PIN_LED_GREEN); }
+static inline void io_led_blue_on(void) { PORTB |= (1 << PIN_LED_BLUE); }
+static inline void io_led_blue_off(void) { PORTB &= ~(1 << PIN_LED_BLUE); }
 
 static inline void io_led_red(bool state) {
 	if (state)
@@ -53,22 +58,17 @@ static inline void io_led_blue(bool state) {
 		io_led_blue_off();
 }
 
-static inline bool io_led_red_state() { return !((PORTC >> PIN_LED_RED) & 0x1); }
-static inline bool io_led_green_state() { return (PORTB >> PIN_LED_GREEN) & 0x1; }
-static inline bool io_led_blue_state() { return (PORTB >> PIN_LED_BLUE) & 0x1; }
+static inline bool io_led_red_state(void) { return !((PORTD >> PIN_LED_RED) & 0x1); }
+static inline bool io_led_green_state(void) { return (PORTB >> PIN_LED_GREEN) & 0x1; }
+static inline bool io_led_blue_state(void) { return (PORTB >> PIN_LED_BLUE) & 0x1; }
 
-static inline void io_led_red_toggle() { io_led_red(!io_led_red_state()); }
-static inline void io_led_green_toggle() { io_led_green(!io_led_green_state()); }
-static inline void io_led_blue_toggle() { io_led_blue(!io_led_blue_state()); }
+static inline void io_led_red_toggle(void) { io_led_red(!io_led_red_state()); }
+static inline void io_led_green_toggle(void) { io_led_green(!io_led_green_state()); }
+static inline void io_led_blue_toggle(void) { io_led_blue(!io_led_blue_state()); }
 
-static inline bool io_button() { return (PIND >> PIN_BUTTON) & 0x1; }
+static inline bool io_button(void) { return (PIND >> PIN_BUTTON) & 0x1; }
 
-void uart_out();
-void uart_in();
-
-static inline void outputs_enable() { PORTD &= ~(1 << PIN_OUTPUTS_DISABLE); }
-static inline void outputs_disable() { PORTD |= (1 << PIN_OUTPUTS_DISABLE); }
-
-void io_shift_update();
+static inline void uart_out(void) { PORTD |= (1 << PIN_UART_DIR); }
+static inline void uart_in(void) { PORTD &= ~(1 << PIN_UART_DIR); }
 
 #endif
