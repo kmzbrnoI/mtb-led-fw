9dd1b568b87abcaced22aa5ee4729ac70aeef6a6
Jan Malina <me@apophis.cz>
2026-06-11 08:49:35 +0200

    Add button debouncing.

---
+++ src/inputs.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+#include "inputs.h"
+#include "io.h"
+
+volatile bool btn_pressed = false;
+
+#define DEBOUNCE_THRESHOLD 20 // 10 ms
+volatile uint8_t _btn_debounce_counter = 0;
+
+void button_debounce_update(void) {
+	bool state = io_button();
+	if (state & 0x01) {
+		if (_btn_debounce_counter > 0) {
+			_btn_debounce_counter--;
+			if ((_btn_debounce_counter == 0) && (btn_pressed)) {
+				btn_pressed = false;
+				btn_on_depressed();
+			}
+		}
+	} else {
+		if (_btn_debounce_counter < DEBOUNCE_THRESHOLD) {
+			_btn_debounce_counter++;
+			if ((_btn_debounce_counter == DEBOUNCE_THRESHOLD) && (!btn_pressed)) {
+				btn_pressed = true;
+				btn_on_pressed();
+			}
+		}
+	}
+}
---
+++ src/inputs.h
@@ -0,0 +1,17 @@
+#ifndef _INPUTS_H_
+#define _INPUTS_H_
+
+/* Button input debouncing.
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+
+extern volatile bool btn_pressed;
+void btn_on_pressed(void);
+void btn_on_depressed(void);
+
+// This function should be called each 100 us
+void button_debounce_update(void);
+
+#endif
