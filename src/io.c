ea63abb0e47bbf029a6fc7acb3f69d99effa461a
Jan Malina <me@apophis.cz>
2026-06-11 08:24:03 +0200

    Migrate IO to MTB-LED HW.

--- src/io.c
+++ src/io.c
@@ -4,101 +4,40 @@
 #include "io.h"
 #include "common.h"
 
+///////////////////////////////////////////////////////////////////////////////
+
 volatile uint16_t _outputs = 0;
-volatile uint16_t _inputs = 0;
-volatile uint8_t _address = 0;
-bool uart_out_high = false;
 
-bool inputs_disabled = false;
+///////////////////////////////////////////////////////////////////////////////
 
-static inline void io_reset() {
-	SPCR = 0;
+static inline void io_reset(void) {
+	SPCR0 = 0;
 	DDRB = 0;
 	DDRC = 0;
 	DDRD = 0;
+	DDRE = 0;
 	PORTB = 0;
 	PORTC = 0;
 	PORTD = 0;
+	PORTE = 0;
 }
 
 void io_init() {
 	// Reset (could load from bootloader)
 	io_reset();
 
-	PORTC |= (1 << PIN_IR_MA); // pull-up pin with inputs-disabled jumper, wait for input to settle
 	DDRB |= (1 << PIN_LED_GREEN) | (1 << PIN_LED_BLUE);
-	DDRC |= (1 << PIN_LED_RED);
-	PORTD |= (1 << PIN_BUTTON) | (1 << PIN_UART_RX); // pull-up
-
-	_delay_us(50); // wait for inputs to load
-
-	// With transistor (active low) = pulled up
-	// Without transistor (active high) = pulled down
-	uart_out_high = !((PIND >> PIN_UART_DIR) & 0x1);
-
-	uart_in();
-	DDRD |= (1 << PIN_TEST_PAD) | (1 << PIN_UART_DIR) | (1 << PIN_UART_TX);
-	io_led_red_off(); // red LED is off in logical one
+	DDRD |= (1 << PIN_LED_RED);
+	DDRE |= (1 << PIN_DEBUG1) | (1 << PIN_DEBUG2);
+	PORTD |= (1 << PIN_BUTTON) | (1 << PIN_UART_RX); // pull-ups
 
 	DDRB |= (1 << PB3) | (1 << PB5) | (1 << PB2); // MOSI & SCK & SS out
 	PORTB |= (1 << PB4); // pull-up on MISO just for sure
-	outputs_disable();
-	DDRD |= (1 << PIN_OUTPUT_SET) | (1 << PIN_INPUT_SHIFT) | (1 << PIN_OUTPUTS_DISABLE);
 
-	SPCR = (1 << SPE) | (1 << MSTR) | (1 << CPOL); // enable SPI, SPI master, frequency=f_osc/4
-
-	inputs_disabled = !((PINC >> PIN_IR_MA) & 0x1);
-	if (!inputs_disabled) {
-		// Configure IR outputs
-		PORTC &= ~(1 << PIN_IR_MA);
-		DDRC |= (1 << PIN_IR_MA) | (1 << PIN_IR_MB) | (1 << PIN_IR_PULSE);
-		PORTC |= (1 << PIN_IR_PULSE); // idle high
-	}
+	//SPCR = (1 << SPE) | (1 << MSTR) | (1 << CPOL); // enable SPI, SPI master, frequency=f_osc/4
 }
 
-bool io_get_input_raw(uint8_t inum) {
-	return (io_get_inputs_raw() >> inum) & 0x1;
-}
-
-uint16_t io_get_inputs_raw() {
-	return _inputs;
-}
-
-void io_set_output_raw(uint8_t onum, bool state) {
-	if (state)
-		_outputs |= (1 << onum);
-	else
-		_outputs &= ~(1 << onum);
-}
-
-void io_set_outputs_raw(uint16_t state) {
-	io_set_outputs_raw_mask(state, 0xFFFF);
-}
-
-void io_set_outputs_raw_mask(uint16_t state, uint16_t mask) {
-	_outputs = (state & mask) | (_outputs & (~mask));
-}
-
-uint16_t io_get_outputs_raw() {
-	return _outputs;
-}
-
-bool io_get_output_raw(uint8_t onum) {
-	return (io_get_outputs_raw() >> onum) & 0x1;
-}
-
-uint8_t io_get_addr_raw() {
-	return _address;
-}
-
-static inline uint8_t switch_bits_03(uint8_t data) {
-	uint8_t res = data & 0xF6;
-	res |= ((data >> 3) & 1);
-	res |= ((data&1) << 3);
-	return res;
-}
-
-void io_shift_update() {
+/*void io_shift_update() {
 	// Typical time of this function: 10 us
 
 	uint16_t buf_inputs = 0;
@@ -143,18 +82,5 @@ void io_shift_load() {
 	__asm__("nop");
 	__asm__("nop");
 	PORTD |= (1 << PIN_INPUT_SHIFT); // disable loading now, data stays loaded in shifts
-}
+}*/
 
-void uart_out() {
-	if (uart_out_high)
-		PORTD |= (1 << PIN_UART_DIR);
-	else
-		PORTD &= ~(1 << PIN_UART_DIR);
-}
-
-void uart_in() {
-	if (uart_out_high)
-		PORTD &= ~(1 << PIN_UART_DIR);
-	else
-		PORTD |= (1 << PIN_UART_DIR);
-}
--- src/io.h
+++ src/io.h
@@ -10,56 +10,43 @@
 #include <stdint.h>
 #include <avr/io.h>
 
-// Defines below are informative only, they are basically never used.
-// If you change IO pins mapping, look into ‹io.c› and change approprite
-// functions.
-
-#define NO_INPUTS 16
-#define NO_OUTPUTS 16
+#define NO_OUTPUTS 32
 #define NO_INPUTS_ADDR 8
 
-#define PIN_LED_RED PC0
+#define PIN_LED_RED PD7
 #define PIN_LED_GREEN PB0
 #define PIN_LED_BLUE PB1
 
-// Warning: button pin conflict with SPI SS
 #define PIN_BUTTON PD5
-#define PIN_TEST_PAD PD4
-
-#define PIN_OUTPUT_SET PD3
-#define PIN_INPUT_SHIFT PD7
-#define PIN_OUTPUTS_DISABLE PD6
-
-#define PIN_IR_MA PC3
-#define PIN_IR_MB PC2
-#define PIN_IR_PULSE PC1
+#define PIN_DEBUG1 PE0
+#define PIN_DEBUG2 PE1
 
 #define PIN_UART_RX PD0
 #define PIN_UART_TX PD1
-#define PIN_UART_DIR PD2
+#define PIN_UART_DIR PD3
 
-#define IR_CHANNELS 4
+#define PIN_VPRG PC1
+#define PIN_DCPRG PD2
+#define PIN_XLAT
+#define PIN_BLANK
+#define PIN_XERR
+#define PIN_GSCLK PD2
 
-void io_init();
-
-bool io_get_input_raw(uint8_t inum);
-uint16_t io_get_inputs_raw();
-
-uint8_t io_get_addr_raw();
+void io_init(void);
 
 void io_set_output_raw(uint8_t onum, bool state);
 void io_set_outputs_raw(uint16_t state);
 void io_set_outputs_raw_mask(uint16_t state, uint16_t mask);
-uint16_t io_get_outputs_raw();
+uint16_t io_get_outputs_raw(void);
 
 bool io_get_output_raw(uint8_t onum);
 
-static inline void io_led_red_on() { PORTC &= ~(1 << PIN_LED_RED); }
-static inline void io_led_red_off() { PORTC |= (1 << PIN_LED_RED); }
-static inline void io_led_green_on() { PORTB |= (1 << PIN_LED_GREEN); }
-static inline void io_led_green_off() { PORTB &= ~(1 << PIN_LED_GREEN); }
-static inline void io_led_blue_on() { PORTB |= (1 << PIN_LED_BLUE); }
-static inline void io_led_blue_off() { PORTB &= ~(1 << PIN_LED_BLUE); }
+static inline void io_led_red_on(void) { PORTD |= (1 << PIN_LED_RED); }
+static inline void io_led_red_off(void) { PORTD &= ~(1 << PIN_LED_RED); }
+static inline void io_led_green_on(void) { PORTB |= (1 << PIN_LED_GREEN); }
+static inline void io_led_green_off(void) { PORTB &= ~(1 << PIN_LED_GREEN); }
+static inline void io_led_blue_on(void) { PORTB |= (1 << PIN_LED_BLUE); }
+static inline void io_led_blue_off(void) { PORTB &= ~(1 << PIN_LED_BLUE); }
 
 static inline void io_led_red(bool state) {
 	if (state)
@@ -82,55 +69,40 @@ static inline void io_led_blue(bool state) {
 		io_led_blue_off();
 }
 
-static inline bool io_led_red_state() { return !((PORTC >> PIN_LED_RED) & 0x1); }
-static inline bool io_led_green_state() { return (PORTB >> PIN_LED_GREEN) & 0x1; }
-static inline bool io_led_blue_state() { return (PORTB >> PIN_LED_BLUE) & 0x1; }
+static inline bool io_led_red_state(void) { return (PORTD >> PIN_LED_RED) & 0x1; }
+static inline bool io_led_green_state(void) { return (PORTB >> PIN_LED_GREEN) & 0x1; }
+static inline bool io_led_blue_state(void) { return (PORTB >> PIN_LED_BLUE) & 0x1; }
 
-static inline void io_led_red_toggle() { io_led_red(!io_led_red_state()); }
-static inline void io_led_green_toggle() { io_led_green(!io_led_green_state()); }
-static inline void io_led_blue_toggle() { io_led_blue(!io_led_blue_state()); }
+static inline void io_led_red_toggle(void) { io_led_red(!io_led_red_state()); }
+static inline void io_led_green_toggle(void) { io_led_green(!io_led_green_state()); }
+static inline void io_led_blue_toggle(void) { io_led_blue(!io_led_blue_state()); }
 
-static inline bool io_button() { return (PIND >> PIN_BUTTON) & 0x1; }
+static inline bool io_button(void) { return (PIND >> PIN_BUTTON) & 0x1; }
 
-static inline void io_testpad_set(bool state) {
+static inline void io_debug1_set(bool state) {
 	if (state)
-		PORTD |= (1 << PIN_TEST_PAD);
+		PORTE |= (1 << PIN_DEBUG1);
 	else
-		PORTD &= ~(1 << PIN_TEST_PAD);
+		PORTE &= ~(1 << PIN_DEBUG1);
 }
 
-static inline bool io_testpad_get() { return ((PORTD >> PIN_TEST_PAD) & 0x1); }
-static inline void io_testpad_toggle() { io_testpad_set(!io_testpad_get()); }
-
-void uart_out();
-void uart_in();
-
-void io_shift_update();
-void io_shift_load();
-
-extern bool inputs_disabled;
-
-static inline void io_ir_pulse(bool state) {
-	// true = IR active, false = IR inactive
+static inline void io_debug2_set(bool state) {
 	if (state)
-		PORTC &= ~(1 << PIN_IR_PULSE);
+		PORTE |= (1 << PIN_DEBUG2);
 	else
-		PORTC |= (1 << PIN_IR_PULSE);
+		PORTE &= ~(1 << PIN_DEBUG2);
 }
 
-static inline void io_ir_channel(uint8_t channel) {
-	if (channel & 1)
-		PORTC |= (1 << PIN_IR_MA);
-	else
-		PORTC &= ~(1 << PIN_IR_MA);
+static inline bool io_debug1_get(void) { return ((PORTE >> PIN_DEBUG1) & 0x1); }
+static inline void io_debug1_toggle(void) { io_debug1_set(!io_debug1_get()); }
 
-	if (channel & 2)
-		PORTC |= (1 << PIN_IR_MB);
-	else
-		PORTC &= ~(1 << PIN_IR_MB);
-}
+static inline bool io_debug2_get(void) { return ((PORTE >> PIN_DEBUG2) & 0x1); }
+static inline void io_debug2_toggle(void) { io_debug2_set(!io_debug2_get()); }
 
-static inline void outputs_enable() { PORTD &= ~(1 << PIN_OUTPUTS_DISABLE); }
-static inline void outputs_disable() { PORTD |= (1 << PIN_OUTPUTS_DISABLE); }
+static inline void uart_out(void) { PORTD |= (1 << PIN_UART_DIR); }
+static inline void uart_in(void) { PORTD &= ~(1 << PIN_UART_DIR); }
+
+//void io_shift_update(void);
+//void io_shift_load(void);
 
 #endif
